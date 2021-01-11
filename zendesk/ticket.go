51a16dfcc5eecdd4b5952557e63f8748aa236d47
Matthew Hupman <mhupman@gmail.com>
2021-01-11 15:47:57 -0500

    Use pointers in Ticket struct values
    Allows for proper Update calls without affecting un-intended fiels.

--- zendesk/ticket.go
+++ zendesk/ticket.go
@@ -47,59 +47,84 @@ func (cf *CustomField) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+func UnwrapInt64(v *int64) int64 {
+    if v != nil {
+        return *v
+    }
+
+    return 0
+}
+
+func WrapInt64(v int64) *int64 {
+    return &v
+}
+
+func UnwrapString(v *string) string {
+    if v != nil {
+        return *v
+    }
+
+    return ""
+}
+
+func WrapString(v string) *string {
+    return &v
+}
+
 type Ticket struct {
 	ID              int64         `json:"id,omitempty"`
-	URL             string        `json:"url,omitempty"`
-	ExternalID      string        `json:"external_id,omitempty"`
-	Type            string        `json:"type,omitempty"`
-	Subject         string        `json:"subject,omitempty"`
-	RawSubject      string        `json:"raw_subject,omitempty"`
-	Description     string        `json:"description,omitempty"`
-	Priority        string        `json:"priority,omitempty"`
-	Status          string        `json:"status,omitempty"`
-	Recipient       string        `json:"recipient,omitempty"`
-	RequesterID     int64         `json:"requester_id,omitempty"`
-	SubmitterID     int64         `json:"submitter_id,omitempty"`
-	AssigneeID      int64         `json:"assignee_id,omitempty"`
-	OrganizationID  int64         `json:"organization_id,omitempty"`
-	GroupID         int64         `json:"group_id,omitempty"`
-	CollaboratorIDs []int64       `json:"collaborator_ids,omitempty"`
-	FollowerIDs     []int64       `json:"follower_ids,omitempty"`
-	EmailCCIDs      []int64       `json:"email_cc_ids,omitempty"`
-	ForumTopicID    int64         `json:"forum_topic_id,omitempty"`
-	ProblemID       int64         `json:"problem_id,omitempty"`
-	HasIncidents    bool          `json:"has_incidents,omitempty"`
-	DueAt           time.Time     `json:"due_at,omitempty"`
-	Tags            []string      `json:"tags,omitempty"`
-	CustomFields    []CustomField `json:"custom_fields,omitempty"`
+	URL             *string        `json:"url,omitempty"`
+	ExternalID      *string        `json:"external_id,omitempty"`
+	Type            *string        `json:"type,omitempty"`
+	Subject         *string        `json:"subject,omitempty"`
+	RawSubject      *string        `json:"raw_subject,omitempty"`
+	Description     *string        `json:"description,omitempty"`
+	Priority        *string        `json:"priority,omitempty"`
+	Status          *string        `json:"status,omitempty"`
+	Recipient       *string        `json:"recipient,omitempty"`
+	RequesterID     *int64         `json:"requester_id,omitempty"`
+	SubmitterID     *int64         `json:"submitter_id,omitempty"`
+	AssigneeID      *int64         `json:"assignee_id,omitempty"`
+	OrganizationID  *int64         `json:"organization_id,omitempty"`
+	GroupID         *int64         `json:"group_id,omitempty"`
+	CollaboratorIDs *[]int64       `json:"collaborator_ids,omitempty"`
+	FollowerIDs     *[]int64       `json:"follower_ids,omitempty"`
+	EmailCCIDs      *[]int64       `json:"email_cc_ids,omitempty"`
+	ForumTopicID    *int64         `json:"forum_topic_id,omitempty"`
+	ProblemID       *int64         `json:"problem_id,omitempty"`
+	HasIncidents    *bool          `json:"has_incidents,omitempty"`
+	DueAt           *time.Time     `json:"due_at,omitempty"`
+	Tags            *[]string      `json:"tags,omitempty"`
+	CustomFields    *[]CustomField `json:"custom_fields,omitempty"`
 
 	Via *Via `json:"via,omitempty"`
+	SatisfactionRating *SatisfactionRating `json:"satisfaction_rating,omitempty"`
 
-	SatisfactionRating struct {
-		ID      int64  `json:"id"`
-		Score   string `json:"score"`
-		Comment string `json:"comment"`
-	} `json:"satisfaction_rating,omitempty"`
-
-	SharingAgreementIDs []int64   `json:"sharing_agreement_ids,omitempty"`
-	FollowupIDs         []int64   `json:"followup_ids,omitempty"`
-	ViaFollowupSourceID int64     `json:"via_followup_source_id,omitempty"`
-	MacroIDs            []int64   `json:"macro_ids,omitempty"`
-	TicketFormID        int64     `json:"ticket_form_id,omitempty"`
-	BrandID             int64     `json:"brand_id,omitempty"`
-	AllowChannelback    bool      `json:"allow_channelback,omitempty"`
-	AllowAttachments    bool      `json:"allow_attachments,omitempty"`
-	IsPublic            bool      `json:"is_public,omitempty"`
-	CreatedAt           time.Time `json:"created_at,omitempty"`
-	UpdatedAt           time.Time `json:"updated_at,omitempty"`
+	SharingAgreementIDs *[]int64   `json:"sharing_agreement_ids,omitempty"`
+	FollowupIDs         *[]int64   `json:"followup_ids,omitempty"`
+	ViaFollowupSourceID *int64     `json:"via_followup_source_id,omitempty"`
+	MacroIDs            *[]int64   `json:"macro_ids,omitempty"`
+	TicketFormID        *int64     `json:"ticket_form_id,omitempty"`
+	BrandID             *int64     `json:"brand_id,omitempty"`
+	AllowChannelback    *bool      `json:"allow_channelback,omitempty"`
+	AllowAttachments    *bool      `json:"allow_attachments,omitempty"`
+	IsPublic            *bool      `json:"is_public,omitempty"`
+	CreatedAt           *time.Time `json:"created_at,omitempty"`
+	UpdatedAt           *time.Time `json:"updated_at,omitempty"`
 
 	// Collaborators is POST only
-	Collaborators Collaborators `json:"collaborators,omitempty"`
+	Collaborators *Collaborators `json:"collaborators,omitempty"`
 
 	// Comment is POST only and required
-	Comment TicketComment `json:"comment,omitempty"`
+	Comment *TicketComment `json:"comment,omitempty"`
 
 	// TODO: TicketAudit (POST only) #126
+}
+
+type SatisfactionRating struct {
+    ID      int64  `json:"id"`
+    Score   string `json:"score"`
+    Comment string `json:"comment"`
 }
 
 // Via is information about source of Ticket or TicketComment
